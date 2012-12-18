d14663e95811043dbe6f5882062f1753c98410f3
Pavel Panchekha <pavpanchekha@gmail.com>
2012-12-18 16:32:08 -0500

    Changed how "too little data" is decided.

--- timing_graph.js
+++ timing_graph.js
@@ -9,32 +9,34 @@ function draw_distroke(ctx, i, x, y) {
 function timing_graph(data) {
     var ctx = document.getElementById("timing_graph").getContext("2d");
 
+    var total = 0;
+    for (var i in data) {
+        total += data[i][1];
+    }
+
     var maxcount = 0;
     var maxavg = 0;
     for (var i in window.data) {
         var count = window.data[i][1];
         var avg   = window.data[i][0] / count;
-        if (count < 5) continue;
+        if (count < .5*Math.sqrt(total)) continue;
         if (count > maxcount) maxcount = count;
         if (avg   > maxavg)   maxavg   = avg;
     }
 
     ctx.beginPath();
     ctx.fillStyle = "#ddd";
-    ctx.font = "6pt monospace";
     for (var i in data) {
         var count = window.data[i][1];
         var avg   = window.data[i][0] / count;
-        if (count >= 5) {
-            // Otherwise, not enough data to be meaningful
-            draw_placeholder(ctx, avg/maxavg, count/maxcount);
-        }
+        draw_placeholder(ctx, avg/maxavg, count/maxcount);
     }
+    ctx.font = "6pt monospace";
     ctx.fillStyle = "rgb(78, 154, 6)"; // Tango dark green
     for (var i in data) {
         var count = window.data[i][1];
         var avg   = window.data[i][0] / count;
-        if (count >= 5) {
+        if (count >= .5*Math.sqrt(total)) {
             // Otherwise, not enough data to be meaningful
             draw_distroke(ctx, i, avg/maxavg, count/maxcount);
         }
